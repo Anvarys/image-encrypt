a286b80136eec8ca7c9bb9bf0eb241b5bdf25949
Anvarys <105349498+Anvarys@users.noreply.github.com>
2026-02-27 10:36:43 +0100

    added image samples to choose from

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { useEffect, useRef, useState } from "react";
 import { Label } from "@/components/ui/label";
 import { toast } from "sonner";
 import { canvas2file, encode_file } from "./encoding";
+import { GithubIcon } from "lucide-react";
+import { Dialog, DialogContent } from "./components/ui/dialog";
 
 
 export function App() {
@@ -12,24 +14,44 @@ export function App() {
   const [canvasContext, setCanvasContext] = useState<CanvasRenderingContext2D | null>(null);
   const [isImageUploaded, setIsImageUploaded] = useState<boolean>(false)
   const [filename, setFilename] = useState<string | null>(null)
+  const [imagesSampleDialogOpen, setImagesSampleDialogOpen] = useState<boolean>(false);
+  const [fileSampleDialogOpen, setFileSampleDialogOpen] = useState<boolean>(false)
+
+  const color_bits_used = 1
 
   async function imageFileInputChange() {
-      const file = imageFileInputRef.current?.files?.[0]
+    const file = imageFileInputRef.current?.files?.[0]
 
-      if (!file || !canvasContext || !canvasRef.current) {setIsImageUploaded(false); return;}
-      if (!file.type.startsWith("image/")) {
-        toast.error("Please upload an image, file you uploaded is not supported", {duration: 8000});
-        imageFileInputRef.current!.value = ""
-        setIsImageUploaded(false)
-        return;
-      }
+    if (!file || !canvasContext || !canvasRef.current) {setIsImageUploaded(false); return;}
+    if (!file.type.startsWith("image/")) {
+      toast.error("Please upload an image, file you uploaded is not supported", {duration: 8000});
+      imageFileInputRef.current!.value = ""
+      setIsImageUploaded(false)
+      return;
+    }
 
-      const bitmap = await createImageBitmap(file);
-      canvasRef.current.height = bitmap.height
-      canvasRef.current.width = bitmap.width
-      canvasContext.drawImage(bitmap, 0, 0)
+    setCanvas(file)
+  }
 
-      setIsImageUploaded(true)
+  async function setCanvas(blob: Blob) {
+    const bitmap = await createImageBitmap(blob);
+    canvasRef!.current!.height = bitmap.height
+    canvasRef!.current!.width = bitmap.width
+    canvasContext!.drawImage(bitmap, 0, 0)
+
+    setIsImageUploaded(true)
+  }
+
+  async function onImageSampleClick(e: React.MouseEvent<HTMLImageElement, MouseEvent>) {
+    const imgElement = e.currentTarget;
+    if (!canvasRef.current || !canvasContext) return;
+
+    canvasRef.current.width = imgElement.naturalWidth; 
+    canvasRef.current.height = imgElement.naturalHeight;
+    canvasContext.drawImage(imgElement, 0, 0)
+
+    setImagesSampleDialogOpen(false)
+    setIsImageUploaded(true)
   }
 
   function fileInputChange() {
@@ -59,7 +81,20 @@ export function App() {
     
     if (!canvasRef.current) return;
 
-    await encode_file(canvasRef.current, file, 1)
+    const result = await encode_file(canvasRef.current, file, color_bits_used)
+
+    if (!result) {return;}
+
+    const url = canvasRef.current.toDataURL("image/png")
+    const anchor = document.createElement("a");
+
+    anchor.href = url;
+    anchor.download = imageFileInputRef.current?.files?.[0]!.name!;
+    document.body.appendChild(anchor);
+    anchor.click();
+    anchor.remove();
+
+    URL.revokeObjectURL(url);
   }
 
   async function decodeButtonClicked() {
@@ -68,7 +103,7 @@ export function App() {
       return;
     }
 
-    const file = await canvas2file(canvasRef.current!)
+    const file = await canvas2file(canvasRef.current!, color_bits_used)
 
     const url = URL.createObjectURL(file);
     const anchor = document.createElement("a");
@@ -80,6 +115,10 @@ export function App() {
     anchor.remove();
 
     URL.revokeObjectURL(url);
+  }
+
+  async function fileSampleClicked(url: string) {
+    
   }
 
   useEffect(() => {
@@ -95,7 +134,7 @@ return (<div className="text-foreground flex flex-col items-center p-4 gap-4">
   }}>
     <div className="items-center flex flex-col">
     Upload an image
-    <Label className="text-center hover:underline cursor-pointer text-muted-foreground">or choose from examples</Label>
+    <Label className="text-center hover:underline cursor-pointer text-muted-foreground" onClick={(e) => {e.stopPropagation(); setImagesSampleDialogOpen(true)}}>or choose from examples</Label>
     </div>
   </Button>
 
@@ -104,17 +143,57 @@ return (<div className="text-foreground flex flex-col items-center p-4 gap-4">
   }}>
     <div className="items-center flex flex-col">
     Upload a file
-    <Label className="text-center hover:underline cursor-pointer text-muted-foreground">or choose from examples</Label>
+    <Label className="text-center hover:underline cursor-pointer text-muted-foreground" onClick={(e) => {e.stopPropagation(); setFileSampleDialogOpen(true)}}>or choose from examples</Label>
     </div>
   </Button>
   
-  <canvas ref={canvasRef} className="max-w-2xl max-h-2xl w-2xl h-2xl border border-4"/>
+  <canvas ref={canvasRef} className="max-w-sm max-h-sm h-sm border border-4" hidden={!isImageUploaded}></canvas>
   {filename && <Label>File: {filename}</Label>}
 
-  <div className="flex flex-row gap-4">
-    <Button className="cursor-pointer text-foreground p-6 text-[1.2rem] border-blue-600 bg-blue-500" onClick={encodeButtonClicked}>Encode</Button>
-    <Button className="cursor-pointer text-foreground p-6 text-[1.2rem]" onClick={decodeButtonClicked}>Decode</Button>
+  <div className="flex flex-col gap-4 w-sm w-max-sm">
+    <div className="flex flex-row gap-4 justify-center">
+    <Button className="cursor-pointer text-foreground p-6 text-[1.2rem] border-violet-600 bg-violet-500 flex-1" onClick={encodeButtonClicked}>Encode</Button>
+    <Button className="cursor-pointer text-foreground p-6 text-[1.2rem] border-green-600 bg-green-500 flex-1" onClick={decodeButtonClicked}>Decode</Button>
+    <a href='https://github.com/Anvarys/image-encrypt' target='_blank'>
+      <div className='bg-neutral-800 p-2 rounded-[0.5rem] border-neutral-700 border flex flex-row w-full h-full items-center'>
+        <GithubIcon className="mr-1"/>
+        <Label className='text-center cursor-pointer'>GitHub</Label>
+      </div>
+    </a>
+    </div>
   </div>
+
+  <Dialog open={imagesSampleDialogOpen} onOpenChange={setImagesSampleDialogOpen}>
+  <DialogContent className="min-w-[80%] max-w-[80%] min-h-[80%] max-h-[80%] bg-neutral-900 border-neutral-800 overflow-auto">
+    <div className="flex flex-wrap gap-10 justify-start items-start content-start">
+    <img src="samples/images/banana_big.png" className="w-full max-w-[384px] h-auto object-contain border-2 rounded-sm border-neutral-900 hover:border-cyan-500 cursor-pointer" onClick={onImageSampleClick}/>
+    <img src="samples/images/cat.jpg" className="w-full max-w-[384px] h-auto object-contain border-2 rounded-sm border-neutral-900 hover:border-cyan-500 cursor-pointer" onClick={onImageSampleClick}/>
+    <img src="samples/images/matrix.jpeg" className="w-full max-w-[384px] h-auto object-contain border-2 rounded-sm border-neutral-900 hover:border-cyan-500 cursor-pointer" onClick={onImageSampleClick}/>
+    <img src="samples/images/infinity.png" className="w-full max-w-[384px] h-auto object-contain border-2 rounded-sm border-neutral-900 hover:border-cyan-500 cursor-pointer" onClick={onImageSampleClick}/>
+    </div>
+  </DialogContent></Dialog>
+
+  <Dialog open={fileSampleDialogOpen} onOpenChange={setFileSampleDialogOpen}>
+  <DialogContent className="min-w-[90%] max-w-[90%] min-h-[80%] max-h-[80%] bg-neutral-900 border-neutral-800 overflow-auto">
+    <div className="flex flex-wrap gap-4 justify-start items-start content-start">
+    <div className="border w-[92.5%] flex flex-col items-center bg-neutral-800 border-2 border-neutral-700 rounded-sm p-2 cursor-pointer hover:border-blue-500">
+      <Label className="text-violet-300 text-[1.1rem]">helloworld.txt<Label className="text-cyan-300 text-[0.8rem]">13 bytes</Label></Label>
+      <p>A plain text file with "hello world!" in it</p>
+    </div>
+    <div className="border w-[92.5%] flex flex-col items-center bg-neutral-800 border-2 border-neutral-700 rounded-sm p-2 cursor-pointer hover:border-blue-500">
+      <Label className="text-violet-300 text-[1.1rem]">banana.png <Label className="text-cyan-300 text-[0.8rem]">22KB</Label></Label>
+      <p>The banana image</p>
+    </div>
+    <div className="border w-[92.5%] flex flex-col items-center bg-neutral-800 border-2 border-neutral-700 rounded-sm p-2 cursor-pointer hover:border-blue-500">
+      <Label className="text-violet-300 text-[1.1rem]">mysterious-audio.mp3<Label className="text-cyan-300 text-[0.8rem]">132KB</Label></Label>
+      <p>Some random audio mp3 file</p>
+    </div>
+    <div className="border w-[92.5%] flex flex-col items-center bg-neutral-800 border-2 border-neutral-700 rounded-sm p-2 cursor-pointer hover:border-blue-500">
+      <Label className="text-violet-300 text-[1.1rem]">logo.png<Label className="text-cyan-300 text-[0.8rem]">1KB</Label></Label>
+      <p>Logo of this website</p>
+    </div>
+    </div>
+  </DialogContent></Dialog>
 </div>);
 }
 
--- src/encoding.ts
+++ src/encoding.ts
@@ -1,12 +1,14 @@
+import { toast } from "sonner";
+
 export async function encode_file(canvas: HTMLCanvasElement, file: File, color_bits_used: number, pixel_offset: number = 0) {
   const pixel_count = canvas.width * canvas.height - pixel_offset;
   const ctx = canvas.getContext("2d");
 
-  if (!ctx) return;
+  if (!ctx) return null;
   
-  if (pixel_count < file.size * (8 / color_bits_used) / 3) {
-    console.log("not enough pixels to encode data");
-    return;
+  if (pixel_count < file.size * (8.0 / color_bits_used) / 3.0) {
+    toast.error(`Not enough pixels on the image to encode the file (${((pixel_count/(file.size * (8.0 / color_bits_used) / 3.0))*100).toFixed(2)}%)`);
+    return null;
   }
 
   const bytes = await file.bytes()
@@ -15,25 +17,31 @@ export async function encode_file(canvas: HTMLCanvasElement, file: File, color_b
   const sizes_bytes_dv = new DataView(sizes_bytes.buffer);
 
   sizes_bytes_dv.setUint32(0, file.size);
+  console.log(file.size)
   sizes_bytes_dv.setUint8(4, file.name.length);
   sizes_bytes_dv.setUint8(5, file.type.length);
 
+  const img_data = ctx.getImageData(0, 0, canvas.width, canvas.height)
+
   pixel_offset += await encode_bytes(canvas, sizes_bytes, color_bits_used, pixel_offset);
-  console.log(pixel_offset)
   pixel_offset += await encode_bytes(canvas, string2Uint8Array(file.name), color_bits_used, pixel_offset);
-  console.log(pixel_offset)
   pixel_offset += await encode_bytes(canvas, string2Uint8Array(file.type), color_bits_used, pixel_offset);
-  console.log(pixel_offset)
   pixel_offset += await encode_bytes(canvas, bytes, color_bits_used, pixel_offset);
+
+  if (pixel_count < pixel_offset) {
+    toast.error(`Not enough pixels on the image to encode the file (${((pixel_count/pixel_offset)*100).toFixed(2)}%)`);
+    ctx.putImageData(img_data, 0, 0);
+    return null;
+  }
 
   return pixel_offset
 }
 
 async function encode_bytes(canvas: HTMLCanvasElement, bytes: Uint8Array, color_bits_used: number, pixel_offset: number, spacing: number = 1): Promise<number> {
   const ctx = canvas.getContext("2d")!;
-  const color_bits_to_remove = 0xff - (0 << color_bits_used) - 1
+  const color_bits_to_remove = 0xff - (1 << color_bits_used) + 1
   const pixel_data_count = 4;
-  
+  const total_pixel_count = canvas.width * canvas.height
   const img_data = ctx.getImageData(0, 0, canvas.width, canvas.height)
   const data: Uint8ClampedArray = img_data.data
 
@@ -41,17 +49,28 @@ async function encode_bytes(canvas: HTMLCanvasElement, bytes: Uint8Array, color_
   let total_bits: number = 0;
 
   while (total_bits < bytes.length*8) {
-    for (let color = 0; color < 3; color++) {
-      data[pixel * pixel_data_count + color] = data[pixel * pixel_data_count + color] & color_bits_to_remove;
+    if (data[pixel * pixel_data_count + 3] === 255) {
+      for (let color = 0; color < 3; color++) {
+        data[pixel * pixel_data_count + color] = data[pixel * pixel_data_count + color] & color_bits_to_remove;
 
-      for (let bit = 0; bit < color_bits_used; bit++) {
-        data[pixel * pixel_data_count + color] += ( ( bytes[(total_bits+bit) >> 3] >> (7 - (total_bits+bit) & 7) ) & 1 ) << (color_bits_used - bit - 1);
+        console.log(data[pixel * pixel_data_count + color].toString(2).padStart(8, "0"))
+
+        for (let bit = 0; bit < color_bits_used; bit++) {
+          data[pixel * pixel_data_count + color] += ( ( bytes[(total_bits+bit) >> 3] >> (7 - (total_bits+bit) & 7) ) & 1 ) << (color_bits_used - bit - 1);
+          console.log(( bytes[(total_bits+bit) >> 3] >> (7 - (total_bits+bit) & 7) ) & 1)
+        }
+
+        console.log(data[pixel * pixel_data_count + color].toString(2).padStart(8, "0"))
+
+        total_bits += color_bits_used
       }
-
-      total_bits += color_bits_used
     }
 
     pixel += spacing;
+
+    if (pixel > total_pixel_count) {
+      return pixel;
+    }
   }
 
   ctx.putImageData(img_data, 0, 0);
@@ -63,30 +82,37 @@ async function decode_part(data: Uint8ClampedArray, byte_count: number, color_bi
   const bytes = new Uint8Array(byte_count);
   const pixel_data_count = 4;
 
+  const total_pixel_count = data.length >> 2
   let pixel = pixel_offset
   let total_bits = 0
 
   while (total_bits < byte_count * 8) {
-    for (let color = 0; color < 3; color++) {
-      for (let bit = 0; bit < color_bits_used; bit++) {
-        bytes[(total_bits + bit) >> 3] += ( data[pixel * pixel_data_count + color] & (1 << bit) ) << (7 - ((total_bits + bit) & 7))
+    if (data[pixel * pixel_data_count + 3] === 255) {
+      for (let color = 0; color < 3; color++) {
+        for (let bit = 0; bit < color_bits_used; bit++) {
+          bytes[(total_bits + bit) >> 3] += ( data[pixel * pixel_data_count + color] & (1 << (color_bits_used - bit - 1)) ) << (7 - ((total_bits + bit) & 7))
 
-        if (total_bits + bit + 1 >= byte_count * 8) break;
+          if (total_bits + bit + 1 >= byte_count * 8) break;
+        }
+
+        total_bits += color_bits_used
       }
-
-      total_bits += color_bits_used
     }
 
     pixel += spacing
+
+    if (pixel > total_pixel_count) {
+      toast.error("An error occured during decoding")
+      console.log(pixel, total_bits, byte_count)
+      throw "not enough pixels"
+    }
   }
 
   return [bytes, pixel];
 }
 
-export async function canvas2file(canvas: HTMLCanvasElement): Promise<File> {
+export async function canvas2file(canvas: HTMLCanvasElement, color_bits_used: number): Promise<File> {
   const data = canvas.getContext("2d")?.getImageData(0, 0, canvas.width, canvas.height).data!;
-
-  const color_bits_used = 1;
 
   const sizes_size = 6;
 
@@ -95,21 +121,18 @@ export async function canvas2file(canvas: HTMLCanvasElement): Promise<File> {
   const [sizes, sizes_pixel_count] = await decode_part(data, sizes_size, color_bits_used, pixel_offset);
   pixel_offset += sizes_pixel_count;
 
-  console.log(pixel_offset)
-
   const sizes_dv = new DataView(sizes.buffer);
   const file_size = sizes_dv.getUint32(0);
+  console.log(file_size)
   const file_name_size = sizes_dv.getUint8(4);
   const file_type_size = sizes_dv.getUint8(5);
 
   const [file_name_bytes, file_name_pixel_count] = await decode_part(data, file_name_size, color_bits_used, pixel_offset);
   pixel_offset += file_name_pixel_count;
-  console.log(pixel_offset)
   const file_name = String.fromCharCode(...file_name_bytes);
 
   const [file_type_bytes, file_type_pixel_count] = await decode_part(data, file_type_size, color_bits_used, pixel_offset);
   pixel_offset += file_type_pixel_count
-  console.log(pixel_offset)
   const file_type = String.fromCharCode(...file_type_bytes);
 
   const [file_bytes,] = await decode_part(data, file_size, color_bits_used, pixel_offset);
